b4b4e30ff196f71ea2b40afd3eb30af9bb76d88c
BaptisteTP <150006584+BaptisteTP@users.noreply.github.com>
2023-12-06 10:20:07 +0100

    interfaces créer

--- UC_Commandes.h
+++ UC_Commandes.h
@@ -35,7 +35,12 @@ namespace POOA2 {
 				delete components;
 			}
 		}
-	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ Supprimer;
+	protected:
+	private: System::Windows::Forms::Button^ Modifier;
+	private: System::Windows::Forms::Button^ Ajouter;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+
 	protected:
 
 	private:
@@ -51,30 +56,85 @@ namespace POOA2 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->Supprimer = (gcnew System::Windows::Forms::Button());
+			this->Modifier = (gcnew System::Windows::Forms::Button());
+			this->Ajouter = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
 			this->SuspendLayout();
 			// 
-			// label1
+			// Supprimer
 			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Century Gothic", 25, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+			this->Supprimer->BackColor = System::Drawing::Color::Red;
+			this->Supprimer->FlatAppearance->BorderSize = 0;
+			this->Supprimer->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Supprimer->Font = (gcnew System::Drawing::Font(L"Candara", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
 				static_cast<System::Byte>(0)));
-			this->label1->ForeColor = System::Drawing::Color::Black;
-			this->label1->Location = System::Drawing::Point(350, 156);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(289, 51);
-			this->label1->TabIndex = 4;
-			this->label1->Text = L"Commandes";
+			this->Supprimer->Location = System::Drawing::Point(823, 281);
+			this->Supprimer->Margin = System::Windows::Forms::Padding(4);
+			this->Supprimer->MinimumSize = System::Drawing::Size(133, 62);
+			this->Supprimer->Name = L"Supprimer";
+			this->Supprimer->Size = System::Drawing::Size(133, 62);
+			this->Supprimer->TabIndex = 7;
+			this->Supprimer->Text = L"Supprimer";
+			this->Supprimer->UseVisualStyleBackColor = false;
+			// 
+			// Modifier
+			// 
+			this->Modifier->BackColor = System::Drawing::Color::DodgerBlue;
+			this->Modifier->FlatAppearance->BorderSize = 0;
+			this->Modifier->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Modifier->Font = (gcnew System::Drawing::Font(L"Candara", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Modifier->Location = System::Drawing::Point(422, 281);
+			this->Modifier->Margin = System::Windows::Forms::Padding(4);
+			this->Modifier->MinimumSize = System::Drawing::Size(133, 62);
+			this->Modifier->Name = L"Modifier";
+			this->Modifier->Size = System::Drawing::Size(133, 62);
+			this->Modifier->TabIndex = 6;
+			this->Modifier->Text = L"Modifier";
+			this->Modifier->UseVisualStyleBackColor = false;
+			// 
+			// Ajouter
+			// 
+			this->Ajouter->BackColor = System::Drawing::Color::LimeGreen;
+			this->Ajouter->FlatAppearance->BorderSize = 0;
+			this->Ajouter->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Ajouter->Font = (gcnew System::Drawing::Font(L"Candara", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Ajouter->Location = System::Drawing::Point(33, 281);
+			this->Ajouter->Margin = System::Windows::Forms::Padding(4);
+			this->Ajouter->MinimumSize = System::Drawing::Size(133, 62);
+			this->Ajouter->Name = L"Ajouter";
+			this->Ajouter->Size = System::Drawing::Size(133, 62);
+			this->Ajouter->TabIndex = 5;
+			this->Ajouter->Text = L"Ajouter";
+			this->Ajouter->UseVisualStyleBackColor = false;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::ButtonFace;
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(33, 20);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersWidth = 51;
+			this->dataGridView1->RowTemplate->Height = 24;
+			this->dataGridView1->Size = System::Drawing::Size(923, 254);
+			this->dataGridView1->TabIndex = 4;
 			// 
 			// UC_Commandes
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->Controls->Add(this->label1);
+			this->BackColor = System::Drawing::Color::Gray;
+			this->Controls->Add(this->Supprimer);
+			this->Controls->Add(this->Modifier);
+			this->Controls->Add(this->Ajouter);
+			this->Controls->Add(this->dataGridView1);
 			this->Name = L"UC_Commandes";
 			this->Size = System::Drawing::Size(988, 363);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
 			this->ResumeLayout(false);
-			this->PerformLayout();
 
 		}
 #pragma endregion
